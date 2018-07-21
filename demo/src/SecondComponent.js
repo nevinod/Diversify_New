3a999a1f064bb5f562f09000b481756051a40d96
Neil Vinod <neilvinod14@gmail.com>
2018-07-20 21:07:44 -0700

    purpose of app

--- demo/src/SecondComponent.js
+++ demo/src/SecondComponent.js
@@ -3,7 +3,13 @@ import React from "react";
 export default () => {
     return (
         <div className="component second-component">
-            <h2>Second Component</h2>
+            <p className="new-explanation"> A common problem in crypto is that most coins are highly correlated. Diversify! allows
+            you to enter a coin, and it will find the coin that is most inversely correlated with yours,
+            allowing you to hedge and diversify your holdings. </p>
+
+            <img className="chart-pic" src="chartimage.png" height="500" width="800" ></img>
         </div>
+
+
     )
 }
